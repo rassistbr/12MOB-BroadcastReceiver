35c21ce8c5b05583e927743304c6304c89e4a589
Rober de Assis Torres <RM31675@fiap.com.br>
2016-11-21 20:38:18 -0200

    Initial commit

---
+++ app/src/main/java/com/example/rm31675/braadcastreceiver/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rm31675.braadcastreceiver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void logar(View v){
+        Intent i = new Intent();
+        i.setAction("android.app.action.ACTION_PASSWORD_SUCCEEDED");
+        sendBroadcast(i);
+    }
+
+}
